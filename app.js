345a91da9581c64bf2da1c60575dddc097cda721
Charles Loxton <charlesloxtonf@gmail.com>
2023-01-02 00:05:40 +0200

    Changed port to 8081

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.post('/upload', (req, res) => {
     })
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
