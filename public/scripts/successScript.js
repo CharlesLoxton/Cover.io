e4d354fbcb2fdf0a38f3555329b004ae58c822a5
Charles Loxton <charlesloxtonf@gmail.com>
2023-01-22 13:11:31 +0200

    Changed Template and Art on website

--- public/scripts/successScript.js
+++ public/scripts/successScript.js
@@ -2,7 +2,8 @@ const resultText = document.getElementById("resultText");
 const par = document.getElementById("outputText");
 const headingName = document.getElementById("headingName");
 const jobHeading = document.getElementById("jobHeading");
-let prevPic = "./assets/images/team1.jpg";
+const btnArea = document.getElementById("saveButtonArea");
+let prevPic = "./assets/images/AIwriting.png";
 let letter = "";
 
 window.onload = function() {
@@ -31,35 +32,43 @@ window.onload = function() {
     let btn = document.createElement("button");
     btn.classList.add("button-style");
     btn.innerHTML = "Save";
+    btn.id = "saveBtn";
     resultText.appendChild(btn);
     btn.addEventListener("click", () => {
-        const doc = new jsPDF();
 
-        doc.setFont("times");
+      const divToCapture = document.querySelector("#pdfOutput");
+      const saveBtn = document.querySelector("#saveBtn");
+      const copyBtn = document.querySelector("#copyBtn");
+      const originalWidth = divToCapture.style.width;
+      
+      saveBtn.style.display = "none";
+      copyBtn.style.display = "none";
+      divToCapture.style.width = "700px"
 
-        const img = document.getElementById('button-image');
+        html2canvas(divToCapture).then(canvas => {
 
-        doc.setFillColor(173, 216, 230); // set the fill color to blue
-        doc.rect(0, 0, doc.internal.pageSize.getWidth(), 60, 'F');
-        doc.addImage(img, 'JPEG', 10, 10, 40, 40);
-        doc.setLineWidth(0.1);
-        doc.setDrawColor(0, 0, 0);
-        doc.rect(10, 10, 40, 40);
+          // Create a new instance of jsPDF
+          const doc = new jsPDF();
 
-        doc.fromHTML(`<h2 style=\"color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif\">
-        ${JSON.parse(name)}</h2><h4 style=\"font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif\">${JSON.parse(job)}</h4>`, 55, 15);
-
-        doc.fromHTML("<h3 style=\"color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif\">Cover Letter</h3>", 90, 60);
-        var strArr = doc.splitTextToSize(letter, 190);
-        doc.text(strArr, 10, 70);
-
-        doc.save("coverletter.pdf");
+          doc.setFillColor(240, 240, 240); 
+          doc.rect(0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), 'F');
+    
+          // Add the image of the div element to the pdf
+          doc.addImage(canvas.toDataURL('image/png'), 'PNG', 12, 20);
+    
+          // Save the pdf
+          doc.save("coverletter.pdf");
+          divToCapture.style.width = originalWidth;
+          saveBtn.style.display = "inline";
+          copyBtn.style.display = "inline";
+        });
         
     });
 
     let copyBtn = document.createElement("button");
     copyBtn.classList.add("button-style");
     copyBtn.style.marginLeft = "10px";
+    copyBtn.id = "copyBtn";
     copyBtn.innerHTML = "Copy";
     resultText.appendChild(copyBtn);
     copyBtn.addEventListener("click", () => {
